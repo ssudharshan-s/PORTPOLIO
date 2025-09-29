288527941c2d97657f0a9fabe8d7785ecdc12808
ssudharshan-s <s.shanfsd@gmail.com>
2025-09-29 09:08:05 +0530

    first commit

--- src/components/ContactAss.jsx
+++ src/components/ContactAss.jsx
@@ -26,6 +26,7 @@ const ContactAss = () => {
         try{
             const response = await fetch("https://portpolio-qabp.onrender.com/api/contact",{
                 method: "POST",
+                
                 headers: { "Content-Type": "application/json"},
                 body: JSON.stringify(formData),
             });
